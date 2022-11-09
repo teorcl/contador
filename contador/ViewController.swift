f0936c451fde32851e2567e01815d2a764412122
Teodoro <teo.calle.lara@gmail.com>
2022-11-09 00:51:16 -0500

    Initial Commit

---
+++ contador/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  contador
+//
+//  Created by TEO on 9/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
